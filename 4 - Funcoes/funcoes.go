d962a6d72de3c80f29004179f1e8210fb1451d3b
Rafael Jardim <jordao.jardim@hotmail.com.br>
2021-01-26 19:44:48 -0300

    funcs

---
+++ 4 - Funcoes/funcoes.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func somar(n1 int8, n2 int8) int8 {
+	return n1 + n2
+}
+
+func calculosMatematicos(n1, n2 int8) (int8, int8) {
+	soma := n1 + n2
+	sub := n1 - n2
+	return soma, sub
+}
+
+func main() {
+	soma := somar(10, 20)
+	fmt.Println(soma)
+
+	var f = func(txt string) string {
+		fmt.Println(txt)
+		return "something else"
+	}
+
+	f("Teste")
+
+	resultadoSoma, _ := calculosMatematicos(10, 15)
+	fmt.Println(resultadoSoma)
+
+}
